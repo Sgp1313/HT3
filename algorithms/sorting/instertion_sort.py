64ee7b7c531a4e7e5f0d1e060479503875b3472e
Sgp1313 <sebastiangarcia@ufm.edu>
2022-05-09 17:09:29 -0600

    insertion

---
+++ algorithms/sorting/instertion_sort.py
@@ -0,0 +1,16 @@
+def linearsearch(arr, n, x):
+    for i in range (0,n):
+        if (arr[i] == x):
+            return i
+    return -1
+
+arr = [1, 2, 3, 4, 5]
+
+x = 1
+n= len(arr)
+position = linearsearch(arr, n, x)
+if (position == -1):
+    print ("element not foud")
+else:
+    print("element is present at index", position)
+
