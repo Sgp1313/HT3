9cf47f19a84c0b4d29fc31d67ae060a9b7f8f843
Sgp1313 <sebastiangarcia@ufm.edu>
2022-05-09 16:58:39 -0600

    divisores de n

---
+++ algorithms/brute_force/divisors_of_n.py
@@ -0,0 +1,12 @@
+
+##divisores de n 
+def divisores(n):
+    ndivisores = []
+    num = n
+    for i in range (1, n+1):
+        if (num%i == 0):
+            ndivisores.append(i)
+    return ndivisores
+x = 50
+result = (divisores(x))
+print("N: "+ str(x)+ " Divisores: " + str(result))
